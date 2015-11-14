4017f1ebf0ad0b648df57d18a8cd4d31a662aae3
Rui Azevedo <ruihfazevedo@gmail.com>
2015-11-14 16:24:59 -0100

    functional document watcher and parser

--- README.md
+++ README.md
@@ -1,51 +1,28 @@
-# lazy-docs
+###lazy-docs###
 Lazy document proxy with watcher
 
-documents are only reaload if changed and when requested, external changes only flag the file to refresh at next request.
+documents only reload if changed and when requested, external changes only flag the file to refresh at next request.
+
+functional documents (a function that returns a document) combines both watching changes and document parsing
 
 **create a pool of text documents**
 ```javascript
-var docs=require('lazy-docs')(function(id){return id;});
-var txt=docs.open("some-text-file.txt");//open with default method (id)
-var xml=docs.open("data/some-xml-file.xml",libxml.parseXmlString);//open with alternative method
+txtDocs=require('lazy-docs')();
+var txtDoc=docs.open("data/test.txt");
+var xmlDoc=docs.open("data/test.xml",libxml.parseXml);//with alternative format
 ```
 **create a pool of libxmljs documents**
 ```javascript
-var xmldocs=require('lazy-docs')(libxml.parseXmlString);
-var xml=xmldocs.open("data/test.xml");
-// subsequent calls to open (of same file) will return the already opened file
-// or a realoaded if file changed
+xmlDocs=require('lazy-docs')(libxml.parseXmlString);
+var xmlDoc=docs.open("data/test.xml");
+console.log(xmlDoc().toString());
 ```
-### API ###
-
-**constructor**
-provide default parse from string method
-
-**.open(url)**
-open document at *url* with default method
-
-**.open(url,fromString)**
-open document at *url* with specific *fromString* method
-
-**.open('file://url'[,fromString])**
-same as above
-
-**.open('mem://url',srcString)**
-use default method to parse *srcString* and store it at memory *url*
-
-**.open('mem://url',srcObjet)**
-store *srcObject* at memory *url*
-
-**.close(url)**
-close file at *url* and quit watching it
-
-** *notes* **
-- memory files are not watched for changes
-- this module does not support multiple methods for the same file
-- docPool can be shared by modules as it does not list contained files
-
-TODO LIST:
+##one liner document request##
+```javascript
+console.log(require("lazy-docs")()("myfile.txt")());
+```
+##TODO LIST:##
 
 - add some assync
 - add more sources (http:,sql:,etc..)
-- optional callback onchange (does OS optimize multi watchers?)
+- optional callback onchange (doesn't OS optimize multi watchers?)
--- lazy-docs.js
+++ lazy-docs.js
@@ -1,17 +1,23 @@
+"use strict";
 /*********
 * Documents proxy with watcher
 * documents are only reaload if changed and when requested
 * external changes only flag the file to refresh at next request
 * Rui Azevedo «neu-rah» (ruihfazevedo@gmail.com) [www.r-site.net]
-
-usage:
-
-    //create a pool of libxmljs documents
-    docs=require('lazy-docs')(libxml.parseXmlString);
-    xmlDoc=docs.open("data/test.xml");
 *****/
-var debug=true;//module.id==="repl";
+process.stdin.resume();
+
+var debug=module.id==="repl";
 var log=debug?console.log:function(){};
+
+function exitHandler(options, err) {
+    if (options.cleanup) console.log('clean');
+    if (err) console.log(err.stack);
+    if (options.exit) process.exit();
+}
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
 
 var expect = require('expect');
 var fs = require('fs');
@@ -21,10 +27,51 @@ if (!String.prototype.startsWith)
   String.prototype.startsWith=function (prefix) {return this.slice(0, prefix.length) == prefix;}
 else expect("oks".startsWith("ok")).toBe(true);
 
-function docPool(fromString) {
-  fromString=fromString||function(id) {return id;}
+module.exports=(()=>{
   var urls={};
-  this.close=function (url) {
+  return (fromString)=> {
+    fromString=fromString||((id)=>id);
+
+    return (url,src)=>{
+      if (!url) return;
+      return ()=> {
+        if (url.startsWith("mem://")) {
+          urls[url].fileName=url;
+          return urls[url].doc=typeof src==="string"?fromString(src):src;
+        }
+        if (url.startsWith("file://")) url=url.substr(7);
+
+        var absFile=path.resolve(process.cwd(), url);
+        if (urls[absFile]) {
+          log("serving from pool");
+          if(urls[absFile].needRefresh) {
+            log("refreshing file (quit watching)",absFile);
+            urls[absFile].watcher.close();
+          } else return urls[absFile].doc;
+        }
+        var tmp=urls[absFile]={};
+        tmp.docLoader=src?src:fromString;
+        log("parsing file",url);
+        tmp.doc=tmp.docLoader(fs.readFileSync(url).toString());;
+        tmp.needRefresh=false;
+        //tmp.needSave=false;
+        tmp.fileName=absFile;
+        log("watching:",absFile);
+        tmp.watcher=fs.watch(absFile, function(event, filename){
+          tmp.needRefresh=true;
+          /*switch(event) {
+            case "change": console.log("File changed ",absFile);break;
+            case "rename": console.log("File renamed ",absFile);break;
+            default: console.log("File event",event,absFile);
+          }*/
+        });
+        return tmp.doc;
+      }
+    }
+  }
+})();
+
+  /*this.close=function (url) {
     if (!url) return;
     var absFile=path.resolve(process.cwd(), url);
     if (urls[absFile]) {
@@ -34,41 +81,13 @@ function docPool(fromString) {
   }
   this.open=function(url,src) {return this.load(url,src).doc;}
   this.load=function(url,src) {
-    if (!url) return;
-    if (url.startsWith("mem://"))
-      return urls[url]=typeof src==="string"?fromString(src):src;
-    if (url.startsWith("file://")) url=url.substr(7);
+  }*/
 
-    var absFile=path.resolve(process.cwd(), url);
-    if (urls[absFile]) {
-      log("serving from pool");
-      if(urls[absFile].needRefresh) {
-        log("refreshing file",absFile);
-        urls[absFile].watcher.close();
-      } else return urls[absFile];
-    }
-    var tmp=urls[absFile]={};
-    tmp.docLoader=src?src:fromString;
-    log("parsing file",url);
-    tmp.doc=tmp.docLoader(fs.readFileSync(url).toString());;
-    tmp.needRefresh=false;
-    //tmp.needSave=false;
-    tmp.fileName=absFile;
-    tmp.watcher=fs.watch(absFile, function(event, filename){
-      tmp.needRefresh=true;
-      switch(event) {
-        case "change": console.log("File changed ",absFile);break;
-        case "rename": console.log("File renamed ",absFile);break;
-        default: console.log("File event",event,absFile);
-      }
-    });
-    return tmp;
-  }
-}
 
-if (module.id="repl") {//debuging with repl inside module [https://github.com/neu-rah/nit]
+if (debug) {//debuging with repl inside module [https://github.com/neu-rah/nit]
   console.log("Debug module lazy-docs loaded into ["+module.id+"] environment")
-  libxml=require('libxmljs');
-  docs=new docPool();//function(id){return id;})
+  var libxml=require('libxmljs');
+  var docs=module.exports(libxml.parseXml);
+  var ctrl=docs("../rxserver/data/ctrl.xml");
 }
-module.exports=function(fromString) {return new docPool(fromString);}
+//module.exports=function(fromString) {return new docPool(fromString);}
